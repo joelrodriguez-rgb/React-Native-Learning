e60e12efc3df841fa066c8cb9cd5b679954d5b50
Joel Rodriguez <rgzjoel14@gmail.com>
2022-11-06 16:42:14 -0300

    v1

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Main = () => {
+    return (
+        <View>
+            <Text>
+                Rate Repository App
+            </Text>
+        </View>
+    )
+}
+
+export default Main;
